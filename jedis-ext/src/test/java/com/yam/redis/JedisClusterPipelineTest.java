ad16ad7bf99af85fd7acc74ba304c438e41f2751
chenjia <1120857870@qq.com>
2021-03-26 15:04:14 +0800

    更新

--- jedis-ext/src/test/java/com/yam/redis/JedisClusterPipelineTest.java
+++ jedis-ext/src/test/java/com/yam/redis/JedisClusterPipelineTest.java
@@ -1,6 +1,6 @@
 /**
- * Copyright: Copyright (c) 2015 
- * 
+ * Copyright: Copyright (c) 2015
+ *
  * @author youaremoon
  * @date 2016年6月25日
  * @version V1.0
@@ -18,19 +18,26 @@ import redis.clients.jedis.HostAndPort;
 import redis.clients.jedis.JedisCluster;
 
 /**
- * @Description: TODO
  * @author youaremoon
+ * @Description: TODO
  * @date 2016年6月25日 上午1:01:21
- *
  */
 public class JedisClusterPipelineTest {
 
-	@Test
-	public void test() {
-		Set<HostAndPort> nodes = new HashSet<HostAndPort>();
-        nodes.add(new HostAndPort("127.0.0.1", 9379));
-        nodes.add(new HostAndPort("127.0.0.1", 9380));
+    @Test
+    public void test() {
+        Set<HostAndPort> nodes = new HashSet<>();
+        nodes.add(new HostAndPort("127.0.0.1", 7000));
+        nodes.add(new HostAndPort("127.0.0.1", 7001));
+        nodes.add(new HostAndPort("127.0.0.1", 7002));
+        nodes.add(new HostAndPort("127.0.0.1", 7003));
+        nodes.add(new HostAndPort("127.0.0.1", 7004));
+        nodes.add(new HostAndPort("127.0.0.1", 7005));
 
+        /*
+        构建 JedisCluster 客户端，传入一系列的地址，依次进行服务(slot)发现操作。如果没有异常，将近根据第一个进行发现操作（注意这里是set，
+        顺序可能与预期不一致）。
+         */
         JedisCluster jc = new JedisCluster(nodes);
 
         long s = System.currentTimeMillis();
@@ -41,7 +48,8 @@ public class JedisClusterPipelineTest {
         try {
             // batch write
             for (int i = 0; i < 10000; i++) {
-                jcp.set("k" + i, "v1" + i);
+                jcp.set("k:" + i, "v1" + i);
+                jcp.expire("k:" + i, 1000);
             }
             jcp.sync();
 
@@ -55,17 +63,16 @@ public class JedisClusterPipelineTest {
         }
 
         // output time 
-        long t = System.currentTimeMillis() - s;
-        System.out.println(t);
+        System.out.println("耗时=" + (System.currentTimeMillis() - s));
 
         System.out.println(batchResult.size());
 
         // 实际业务代码中，close要在finally中调，这里之所以没这么写，是因为懒
         try {
-			jc.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+            jc.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
